5d5d5812e3ca7453761e60136749a01247496999
jongdu <parksn5029@naver.com>
2023-09-22 16:42:27 +0900

    초기 프로젝트 세팅

---
+++ src/main/java/com/kr/matitting/MatittingApplication.java
@@ -0,0 +1,13 @@
+package com.kr.matitting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatittingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatittingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kr/matitting/MatittingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kr.matitting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatittingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
