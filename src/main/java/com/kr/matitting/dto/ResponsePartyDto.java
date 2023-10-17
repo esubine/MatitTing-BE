d939f069fde2d7a668a8e98274d4ef5f0f79c869
jongdu <parksn5029@naver.com>
2023-10-18 08:54:51 +0900

    Feat : 파티 디테일 & 파티 삭제 기능 구현

---
+++ src/main/java/com/kr/matitting/dto/ResponsePartyDto.java
@@ -0,0 +1,55 @@
+package com.kr.matitting.dto;
+
+import com.kr.matitting.constant.Gender;
+import com.kr.matitting.constant.PartyAge;
+import com.kr.matitting.constant.PartyStatus;
+import com.kr.matitting.entity.Party;
+import jakarta.validation.constraints.NotNull;
+
+import java.time.LocalDateTime;
+
+public record ResponsePartyDto(
+        @NotNull
+        String partyTitle,
+        @NotNull
+        String partyContent,
+        @NotNull
+        String address,
+        @NotNull
+        String longitude,
+        @NotNull
+        String latitude,
+        @NotNull
+        PartyStatus status,
+        @NotNull
+        Gender gender,
+        @NotNull
+        PartyAge age,
+        @NotNull
+        LocalDateTime deadline,
+        @NotNull
+        LocalDateTime partyTime,
+        @NotNull
+        Integer totalParticipate,
+        @NotNull
+        Integer participate,
+        @NotNull
+        String thumbnail
+) {
+    public static ResponsePartyDto toDto(Party party) {
+        return new ResponsePartyDto(
+                party.getPartyTitle(),
+                party.getPartyContent(),
+                party.getAddress(),
+                party.getLongitude(),
+                party.getLatitude(),
+                party.getStatus(),
+                party.getGender(),
+                party.getAge(),
+                party.getDeadline(),
+                party.getPartyTime(),
+                party.getTotalParticipant(),
+                party.getParticipantCount(),
+                party.getThumbnail());
+    }
+}
