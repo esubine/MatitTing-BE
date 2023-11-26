846d978688e3d7afcf7aae21aaa22744c1533126
jongdu <parksn5029@naver.com>
2023-11-26 15:20:33 +0900

    [mod] pagination -> no off set 방식으로 로직 변경

--- src/main/java/com/kr/matitting/repository/PartyRepositoryCustom.java
+++ src/main/java/com/kr/matitting/repository/PartyRepositoryCustom.java
@@ -2,9 +2,9 @@ package com.kr.matitting.repository;
 
 import com.kr.matitting.dto.PartySearchCondDto;
 import com.kr.matitting.entity.Party;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 
 public interface PartyRepositoryCustom {
-    Page<Party> searchPage(PartySearchCondDto partySearchCondDto, Pageable pageable);
+    Slice<Party> searchPage(PartySearchCondDto partySearchCondDto, Pageable pageable, Long lastPartyId);
 }
--- src/main/java/com/kr/matitting/service/SearchService.java
+++ src/main/java/com/kr/matitting/service/SearchService.java
@@ -1,19 +1,19 @@
 package com.kr.matitting.service;
 
-import com.kr.matitting.dto.PartyCreateDto;
-import com.kr.matitting.dto.PartySearchCondDto;
-import com.kr.matitting.dto.ResponseRankingDto;
-import com.kr.matitting.dto.ResponseSearchDto;
+import com.kr.matitting.constant.Orders;
+import com.kr.matitting.dto.*;
+import com.kr.matitting.entity.Party;
 import com.kr.matitting.repository.PartyRepositoryCustom;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ZSetOperations;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Comparator;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -26,16 +26,24 @@ public class SearchService {
     private final RedisTemplate<String, String> redisTemplate;
     private final PartyRepositoryCustom partyRepositoryCustom;
 
-    public List<ResponseSearchDto> getPartyPage(PartySearchCondDto partySearchCondDto, Pageable pageable) {
+    public ResponseSearchPageDto getPartyPage(PartySearchCondDto partySearchCondDto, Integer size, Long lastPartyId) {
         if (!(partySearchCondDto.title() == null)) {
             increaseKeyWordScore(partySearchCondDto.title());
         }
         if (!(partySearchCondDto.menu() == null)) {
             increaseKeyWordScore(partySearchCondDto.menu());
         }
-        List<ResponseSearchDto> partyList = partyRepositoryCustom.searchPage(partySearchCondDto, pageable).stream()
-                .map(party -> ResponseSearchDto.toDto(party)).sorted(Comparator.comparing(ResponseSearchDto::getHit)).toList();
-        return partyList;
+        Sort sort = partySearchCondDto.sortDto().getOrders() == Orders.DESC
+                ? Sort.by(partySearchCondDto.sortDto().getSorts().getKey()).descending()
+                : Sort.by(partySearchCondDto.sortDto().getSorts().getKey()).ascending();
+
+        PageRequest pageable = PageRequest.of(0, size, sort);
+
+        Slice<Party> partySlice = partyRepositoryCustom.searchPage(partySearchCondDto, pageable, lastPartyId);
+        List<ResponsePartyDto> responsePartyList = partySlice.stream().map(ResponsePartyDto::toDto).collect(Collectors.toList());
+        Long newLastPartyId = getLastPartyId(responsePartyList);
+
+        return new ResponseSearchPageDto(responsePartyList, newLastPartyId, partySlice.hasNext());
     }
     public void increaseKeyWordScore(String keyWord) {
         log.info("=== increaseKeyWordScore() start ===");
@@ -56,5 +64,9 @@ public class SearchService {
         ZSetOperations<String, String> ZSetOperations = redisTemplate.opsForZSet();
         Set<ZSetOperations.TypedTuple<String>> typedTuples = ZSetOperations.reverseRangeWithScores(key, 0, 9);
         return typedTuples.stream().map(set -> new ResponseRankingDto(set.getValue(), set.getScore())).collect(Collectors.toList());
+    }
+
+    private Long getLastPartyId(List<ResponsePartyDto> responsePartyList){
+        return responsePartyList.isEmpty() ? null : responsePartyList.get(responsePartyList.size() - 1).partyId();
     }
 }
