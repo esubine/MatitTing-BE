77fc0715f5aab2fada7bcb074ef1baf8416df36d
subin <lsb1026@naver.com>
2023-12-12 21:09:39 +0900

    feat: 메인 페이지 조회 로직 변경 & 정렬 조건 추가

--- src/main/java/com/kr/matitting/service/MainService.java
+++ src/main/java/com/kr/matitting/service/MainService.java
@@ -1,5 +1,7 @@
 package com.kr.matitting.service;
 
+import com.kr.matitting.constant.PartyStatus;
+import com.kr.matitting.constant.Sorts;
 import com.kr.matitting.dto.*;
 import com.kr.matitting.entity.Party;
 import com.kr.matitting.exception.Map.MapException;
@@ -12,6 +14,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Slf4j
@@ -19,32 +22,30 @@ import java.util.stream.Collectors;
 @Transactional
 @RequiredArgsConstructor
 public class MainService {
-    private final int MAX_DISTANCE = 20;
     private final double DEFAULT_LATITUDE = 37.566828706631135;
     private final double DEFAULT_LONGITUDE = 126.978646598009;
     private final PartyRepositoryImpl partyRepositoryImpl;
 
-    public ResponseMainPageDto getPartyList(MainPageDto mainPageDto, Pageable pageable, Long lastPartyId) {
+    public ResponseMainPageDto getPartyList(MainPageDto mainPageDto, PartyStatus partyStatus, Pageable pageable, Long lastPartyId, Sorts sort) {
         log.info("=== getPartyList() start ===");
-        CalculateDto calculateDto;
-
-        if (mainPageDto.getLatitude() == null | mainPageDto.getLongitude() == null) {
-            calculateDto = calculate(DEFAULT_LONGITUDE, DEFAULT_LATITUDE);
+        // 유저 위치 정보
+        double userLat;
+        if(mainPageDto.getLatitude() == null){
+            userLat = DEFAULT_LATITUDE;
         } else {
-            calculateDto = calculate(mainPageDto.getLongitude(), mainPageDto.getLatitude());
+            userLat = mainPageDto.getLatitude();
+        }
+        double userLon;
+        if(mainPageDto.getLongitude() == null){
+            userLon = DEFAULT_LONGITUDE;
+        } else {
+            userLon = mainPageDto.getLongitude();
         }
 
-        if (calculateDto.getMinLatitude() < 0 || calculateDto.getMaxLatitude() < 0
-                || calculateDto.getMinLongitude() < 0 || calculateDto.getMaxLongitude() < 0) {
-            throw new MapException(com.kr.matitting.exception.main.MainExceptionType.INVALID_COORDINATE);
-        }
+        Slice<Party> partyList = partyRepositoryImpl.getPartyList(userLat, userLon, partyStatus, sort, lastPartyId, pageable);
 
-        Slice<Party> partyList = partyRepositoryImpl.getPartyList(calculateDto.getMinLatitude(), calculateDto.getMaxLatitude(),
-                                                                calculateDto.getMinLongitude(), calculateDto.getMaxLongitude(),
-                                                                pageable, lastPartyId);
-
-        List<ResponsePartyDto> responsePartyList = partyList.stream()
-                .map(ResponsePartyDto::toDto)
+        List<ResponseMainPartyListDto> responsePartyList = partyList.stream()
+                .map(ResponseMainPartyListDto::toDto)
                 .collect(Collectors.toList());
 
         Long newLastPartyId = getLastPartyId(responsePartyList);
@@ -53,19 +54,7 @@ public class MainService {
         return new ResponseMainPageDto(responsePartyList, responsePageInfoDto);
     }
 
-    // 유저의 위도, 경도를 바탕으로 반경 10km 위도, 경도값 계산
-    private CalculateDto calculate(double userLongitude, double userLatitude) {
-        double earthRadius = 6371; // 지구 반지름 (단위: km)
-
-        double minLat = userLatitude - (MAX_DISTANCE / earthRadius) * (180.0 / Math.PI);
-        double maxLat = userLatitude + (MAX_DISTANCE / earthRadius) * (180.0 / Math.PI);
-        double minLon = userLongitude - (MAX_DISTANCE / earthRadius) * (180.0 / Math.PI);
-        double maxLon = userLongitude + (MAX_DISTANCE / earthRadius) * (180.0 / Math.PI);
-
-        return new CalculateDto(minLat, maxLat, minLon, maxLon);
-    }
-
-    private Long getLastPartyId(List<ResponsePartyDto> responsePartyList){
+    private Long getLastPartyId(List<ResponseMainPartyListDto> responsePartyList){
         Long id;
         if(responsePartyList.isEmpty()){
             id = null;
