325a16394b97064d7a3091a0f60cc462c1ed412a
subin <lsb1026@naver.com>
2023-12-17 15:14:28 +0900

    mod: main page response dto 수정, swagger 수정

--- src/main/java/com/kr/matitting/dto/ResponseMainPageDto.java
+++ src/main/java/com/kr/matitting/dto/ResponseMainPageDto.java
@@ -8,7 +8,7 @@ import java.util.List;
 @Schema(description = "메인페이지 Response")
 @Getter
 public class ResponseMainPageDto {
-    @Schema(description = "메인 페이지 party List", type = "array", implementation = ResponsePartyDto.class)
+    @Schema(description = "메인 페이지 party List", type = "array", implementation = ResponseMainPartyListDto.class)
     private List<ResponseMainPartyListDto> partyList;
     @Schema(description = "메인 페이지의 페이지 정보", implementation = ResponsePageInfoDto.class)
     private ResponsePageInfoDto pageInfo;
