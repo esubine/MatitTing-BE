c4c3020c09b0377766fd6d930a94119701b1c288
jongdu <parksn5029@naver.com>
2023-12-18 23:51:26 +0900

    [mod] enum 값 변경

--- src/main/java/com/kr/matitting/constant/PartyJoinStatus.java
+++ src/main/java/com/kr/matitting/constant/PartyJoinStatus.java
@@ -5,7 +5,7 @@ import lombok.Getter;
 @Getter
 public enum PartyJoinStatus {
     //파티 참가 수락/대기/거절
-    ACCEPT("ACCEPT"), WAIT("WAIT"), REFUSE("REFUSE");
+    APPLY("APPLY"), CANCEL("CANCEL");
 
     PartyJoinStatus(String key) {
         this.key = key;
