0d81e13fe22ebc3bb6ef1d3fe21e435939a39b48
jongdu <parksn5029@naver.com>
2024-01-14 15:14:37 +0900

    [fix] ResponseRankingDto 수정

--- src/main/java/com/kr/matitting/dto/ResponseRankingDto.java
+++ src/main/java/com/kr/matitting/dto/ResponseRankingDto.java
@@ -1,8 +1,10 @@
 package com.kr.matitting.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
 public class ResponseRankingDto {
+    @JsonProperty
     private String keyword;
 }
