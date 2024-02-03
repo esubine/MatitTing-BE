fa2314708027bf697aab4f0dadb3ab57105be5d7
subin <lsb1026@naver.com>
2024-02-03 21:14:13 +0900

    Feat: 채팅 관련 메서드 수정, 파티 서비스 eventPublisher 추가

--- src/main/java/com/kr/matitting/service/ChatService.java
+++ src/main/java/com/kr/matitting/service/ChatService.java
@@ -1,13 +1,16 @@
 package com.kr.matitting.service;
 
-import com.kr.matitting.constant.ChatRoomType;
-import com.kr.matitting.entity.*;
+import com.kr.matitting.dto.ChatMessage;
+import com.kr.matitting.dto.ResponseChatDto;
+import com.kr.matitting.dto.ResponseChatRoomListDto;
+import com.kr.matitting.dto.ResponseChatRoomUserDto;
+import com.kr.matitting.entity.ChatRoom;
+import com.kr.matitting.entity.ChatUser;
+import com.kr.matitting.entity.Party;
+import com.kr.matitting.entity.User;
 import com.kr.matitting.exception.chat.ChatException;
-import com.kr.matitting.exception.chat.ChatExceptionType;
 import com.kr.matitting.exception.party.PartyException;
-import com.kr.matitting.exception.party.PartyExceptionType;
 import com.kr.matitting.exception.user.UserException;
-import com.kr.matitting.exception.user.UserExceptionType;
 import com.kr.matitting.repository.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
@@ -21,88 +24,68 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
-import static com.kr.matitting.constant.ChatRoomType.*;
-import static com.kr.matitting.constant.ChatUserRole.*;
-import static com.kr.matitting.dto.ChatHistoryDto.*;
-import static com.kr.matitting.dto.ChatRoomDto.*;
-import static com.kr.matitting.dto.ChatUserDto.*;
-import static com.kr.matitting.entity.ChatRoom.*;
+import static com.kr.matitting.constant.Role.HOST;
+import static com.kr.matitting.constant.Role.VOLUNTEER;
+import static com.kr.matitting.dto.ChatRoomDto.CreateRoomEvent;
+import static com.kr.matitting.dto.ChatRoomDto.JoinRoomEvent;
+import static com.kr.matitting.entity.ChatRoom.createRoom;
 import static com.kr.matitting.exception.chat.ChatExceptionType.*;
-import static com.kr.matitting.exception.party.PartyExceptionType.*;
-import static com.kr.matitting.exception.user.UserExceptionType.*;
+import static com.kr.matitting.exception.party.PartyExceptionType.NOT_FOUND_PARTY;
+import static com.kr.matitting.exception.user.UserExceptionType.INVALID_ROLE_USER;
+import static com.kr.matitting.exception.user.UserExceptionType.NOT_FOUND_USER;
 
 @Service
 @RequiredArgsConstructor
 public class ChatService {
     private final ChatUserRepository chatUserRepository;
     private final ChatRoomRepository chatRoomRepository;
-    private final ChatHistoryRepository chatHistoryRepository;
+    private final ChatRoomRepositoryImpl chatRoomRepositoryImpl;
     private final PartyRepository partyRepository;
     private final UserRepository userRepository;
     private final SimpMessageSendingOperations messagingTemplate;
-    private final ChatRepositoryCustom chatRepositoryCustom;
 
+    // 내 전체 채팅방 조회
     @Transactional(readOnly = true)
-    public List<ChatRoomItem> getChatRooms(Long userId, ChatRoomType roomType, Long lastId, Pageable pageable) {
-        List<ChatUser> chatUsers = chatUserRepository.findByUserIdAndRoomTypeFJRoom(userId, roomType, pageable);
-
-        if (chatUsers == null) return null;
-
-        return chatUsers.stream()
-                .map(ChatRoomItem::new)
-                .toList();
-    }
-
-    @Transactional(readOnly = true)
-    public List<ChatHistoryResponse> getHistories(Long userId, Long roomId, Long lastHistoryId, Pageable pageable) {
-        ChatUser chatUser = chatUserRepository.findByUserIdAndChatRoomId(userId, roomId).orElseThrow();
-        List<ChatHistory> chatHistories = chatRepositoryCustom.getHistories(chatUser.getId(), roomId, lastHistoryId, pageable);
-
-        if (chatHistories == null) return null;
-
-        return chatHistories.stream()
-                .map(ChatHistoryResponse::new)
-                .toList();
-    }
-
-    @Transactional
-    public void requestOneOnOne(Long userId, Long partyId) {
-        Optional<ChatUser> chatRoomOptional = chatUserRepository.findByPartyIdFJChatRoom(partyId);
-        Party party = partyRepository.findByIdFJUser(partyId).orElseThrow(
-                () -> new PartyException(NOT_FOUND_PARTY)
-        );
-        User user = userRepository.findById(userId).orElseThrow(
-                () -> new UserException(NOT_FOUND_USER)
-        );
-
-        if (chatRoomOptional.isPresent()) {
-            throw new ChatException(NOT_FOUND_CHAT_ROOM);
+    public ResponseChatRoomListDto getChatRooms(Long userId, Long lastId, Pageable pageable) {
+        if (chatUserRepository.findByUserId(userId).isEmpty()) {
+            throw new ChatException(IS_NOT_HAVE_CHAT_ROOM);
         }
 
-        ChatRoom room = createRoom(party, user, PRIVATE, party.getPartyTitle());
-        chatRoomRepository.save(room);
+        LocalDateTime time = (lastId == 0L) ? null : getModifiedDate(lastId);
+        return chatRoomRepositoryImpl.getChatRooms(userId, time, pageable);
+    }
 
-        ChatUser chatOwner = ChatUser.createChatUser(room, party.getUser(), ADMIN, PRIVATE);
-        ChatUser chatParticipant = ChatUser.createChatUser(room, party.getUser(), PARTICIPANT, PRIVATE);
-
-        chatUserRepository.save(chatOwner);
-        chatUserRepository.save(chatParticipant);
+    private LocalDateTime getModifiedDate(Long lastId) {
+        Optional<ChatRoom> chatRoom = chatRoomRepository.findById(lastId);
+        if (chatRoom.isPresent()) {
+            return chatRoom.get().getModifiedDate();
+        } else {
+            throw new ChatException(NOT_FOUND_CHAT_ROOM);
+        }
     }
 
     @Transactional(readOnly = true)
-    public List<ChatRoomItem> searchChatRoom(Long userId, String name) {
-        List<ChatRoom> chatRooms = chatRoomRepository.findByUserIdAndTitleLike(userId, name);
+    public List<ResponseChatDto> getChats(Long userId, Long roomId, Long lastChatId, Pageable pageable) {
+        ChatRoom chatRoom = chatRoomRepository.findById(roomId).orElseThrow(() -> new ChatException(NOT_FOUND_CHAT_ROOM));
 
-        return chatRooms.stream()
-                .map(ChatRoomItem::new)
-                .toList();
+        return chatRoom.getChatList().stream()
+                .map(chat -> {
+                    ChatUser user = chat.getSendUser();
+                    return ResponseChatDto.builder()
+                            .senderId(user.getId())
+                            .nickname(user.getNickname())
+                            .message(chat.getMessage())
+                            .createAt(chat.getCreateDate())
+                            .build();
+                }).toList();
     }
 
+    // 채팅방 유저 강퇴 - 방장만 가능
     @Transactional
     public void evictUser(Long userId, Long targetId, Long roomId) {
         ChatUser owner = chatUserRepository.findByUserIdAndChatRoomId(userId, roomId).orElseThrow();
 
-        if (owner.getUserRole().equals(ADMIN)) {
+        if (owner.getUserRole().equals(HOST)) {
             ChatUser participant = chatUserRepository.findByUserIdAndChatRoomId(targetId, roomId).orElseThrow(
                     () -> new ChatException(NOT_FOUND_CHAT_USER_INFO)
             );
@@ -112,20 +95,21 @@ public class ChatService {
         }
     }
 
+    // 채팅방 내 유저들의 정보 조회
     @Transactional(readOnly = true)
-    public ChatUserInfoResponse getRoomUsers(Long roomId, Long userId) {
+    public List<ResponseChatRoomUserDto> getRoomUsers(Long roomId, Long userId) {
+        chatRoomRepository.findById(roomId).orElseThrow(() -> new ChatException(NOT_FOUND_CHAT_ROOM));
+
+        chatUserRepository.findByUserIdAndChatRoomId(userId, roomId).orElseThrow(() -> new ChatException(NOT_FOUND_USER));
+
         List<ChatUser> chatUsers = chatUserRepository.findByChatRoomId(roomId);
 
-        ChatUser chatUser = chatUsers.stream()
-                .filter(cu -> cu.getUser().getId().equals(userId))
-                .findAny()
-                .orElseThrow(() -> new ChatException(NO_PRINCIPAL));
-
-        chatUsers.removeIf(a -> a.getUser().getId().equals(userId));
-
-        return new ChatUserInfoResponse(chatUser, chatUsers);
+        return chatUsers.stream()
+                .map(ResponseChatRoomUserDto::new)
+                .toList();
     }
 
+    //파티방 생성 - 파티 글 생성 완료 시 실행
     @TransactionalEventListener(phase = TransactionPhase.BEFORE_COMMIT)
     public void createChatRoom(CreateRoomEvent createRoomEvent) {
         Party party = partyRepository.findById(createRoomEvent.getPartyId()).orElseThrow(
@@ -134,10 +118,10 @@ public class ChatService {
         User user = userRepository.findById(createRoomEvent.getUserId()).orElseThrow(
                 () -> new UserException(NOT_FOUND_USER)
         );
-        ChatRoom room = createRoom(party, user, GROUP, party.getPartyTitle());
+        ChatRoom room = createRoom(party, user, party.getPartyTitle());
         chatRoomRepository.save(room);
 
-        ChatUser chatUser = ChatUser.createChatUser(room, user, ADMIN, GROUP);
+        ChatUser chatUser = ChatUser.createChatUser(room, user, HOST);
         chatUserRepository.save(chatUser);
     }
 
@@ -149,14 +133,23 @@ public class ChatService {
     @Transactional
     public void sendMessage(Long userId, ChatMessage chatMessage) {
         Long roomId = chatMessage.getRoomId();
-        chatUserRepository.findByUserIdAndChatRoomId(userId, roomId).orElseThrow(
-                () -> new ChatException(NO_PRINCIPAL)
-        );
-
-        chatRoomRepository.findById(roomId).ifPresent(
-                room -> room.setModifiedDate(LocalDateTime.now())
-        );
+        ChatRoom chatRoom = chatRoomRepository.findById(roomId).orElseThrow(() -> new ChatException(NOT_FOUND_CHAT_ROOM));
+        chatRoom.setModifiedDate(LocalDateTime.now());
+        chatRoom.getChatUserList().stream().map(user -> user.getUser().getId().equals(userId)).findAny().orElseThrow(() -> new UserException(INVALID_ROLE_USER));
 
         messagingTemplate.convertAndSend("/sub/chat/room/" + chatMessage.getRoomId(), chatMessage);
+    }
+
+    // 파티 참가 요청 수락 시 채팅유저에 정보 추가
+    @TransactionalEventListener(phase = TransactionPhase.BEFORE_COMMIT)
+    public void addParticipant(JoinRoomEvent joinRoomEvent) {
+
+        ChatRoom chatRoom = chatRoomRepository.findByPartyId(joinRoomEvent.getPartyId()).orElseThrow(() -> new ChatException(NOT_FOUND_CHAT_ROOM));
+
+        User user = userRepository.findById(joinRoomEvent.getUserId()).orElseThrow(() -> new UserException(NOT_FOUND_USER));
+
+        ChatUser chatUser = ChatUser.createChatUser(chatRoom, user, VOLUNTEER);
+
+        chatUserRepository.save(chatUser);
     }
 }
