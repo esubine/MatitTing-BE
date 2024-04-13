9f17f4522c6fe841623339dbf0f7b45ac1d508ef
jongdu <parksn5029@naver.com>
2024-04-13 15:04:57 +0900

    [mod] 파티 현황 Swagger 추가 보충

--- src/main/java/com/kr/matitting/dto/PartyStatusReq.java
+++ src/main/java/com/kr/matitting/dto/PartyStatusReq.java
@@ -2,6 +2,7 @@ package com.kr.matitting.dto;
 
 import com.kr.matitting.constant.PartyStatus;
 import com.kr.matitting.constant.Role;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -13,7 +14,9 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 public class PartyStatusReq {
+    @Schema(description = "유저의 Role에 따라서 방장으로 속한 파티 or 참여하고 있는 파티를 Response 해주는 Column", example = "HOST or VOLUNTEER")
     @NotNull
     private Role role;
+    @Schema(description = "파티 상태 Filter, 모집중 or 모집완료 or 파티종료", example = "RECRUIT or RECRUIT_FINISH or PARTY_FINISH")
     private PartyStatus status = PartyStatus.RECRUIT;
 }
