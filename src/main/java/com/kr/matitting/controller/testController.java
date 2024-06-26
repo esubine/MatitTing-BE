cbe7a5b8e31917a8de2f8a51ae33d85b960c7842
jongdu <parksn5029@naver.com>
2024-06-26 15:17:13 +0900

    [mod] 더미 데이터 데이터 무결성 이슈 수정

--- src/main/java/com/kr/matitting/controller/testController.java
+++ src/main/java/com/kr/matitting/controller/testController.java
@@ -19,7 +19,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.time.LocalDateTime;
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
 
 @Controller
 @RequiredArgsConstructor
@@ -110,8 +112,14 @@ public class testController {
             "    - 파티 생성\n\n" +
             "    - 사용자1 파티에 참가 신청 \n\n"
     )
-    @GetMapping("/matitting3")
+    @PostMapping("/matitting3")
     public ResponseEntity<ResponseDummyDataDto> test(HttpServletResponse response) {
+        Optional<User> bySocialId_01 = userRepository.findBySocialId("3035953918");
+        Optional<User> bySocialId_02 = userRepository.findBySocialId("213312213");
+
+        bySocialId_01.ifPresent(user -> userRepository.deleteById(user.getId()));
+        bySocialId_02.ifPresent(user -> userRepository.deleteById(user.getId()));
+
         User user1 = User.builder()
                 .socialId("3035953918")
                 .oauthProvider(OauthProvider.KAKAO)
@@ -221,6 +229,14 @@ public class testController {
             "\t속한 파티 -> 파티1, 파티2")
     @PostMapping("/matitting4")
     public ResponseEntity<Map<String, Object>> testPartyStatus() {
+        Optional<User> bySocialId_01 = userRepository.findBySocialId("1");
+        Optional<User> bySocialId_02 = userRepository.findBySocialId("2");
+        Optional<User> bySocialId_03 = userRepository.findBySocialId("3");
+
+        bySocialId_01.ifPresent(user -> userRepository.deleteById(user.getId()));
+        bySocialId_02.ifPresent(user -> userRepository.deleteById(user.getId()));
+        bySocialId_03.ifPresent(user -> userRepository.deleteById(user.getId()));
+
         User user1 = User.builder()
                 .socialId("1")
                 .oauthProvider(OauthProvider.NAVER)
@@ -306,25 +322,25 @@ public class testController {
                 .thumbnail("유저2 증명사진.jpg")
                 .build();
 
-        ResponseCreatePartyDto p1 = partyService.createParty(u1, partyCreateDto1);
-        ResponseCreatePartyDto p2 = partyService.createParty(u1, partyCreateDto2);
-        ResponseCreatePartyDto p3 = partyService.createParty(u2, partyCreateDto3);
+        ResponseCreatePartyDto p1 = partyService.createParty(u1.getId(), partyCreateDto1);
+        ResponseCreatePartyDto p2 = partyService.createParty(u1.getId(), partyCreateDto2);
+        ResponseCreatePartyDto p3 = partyService.createParty(u2.getId(), partyCreateDto3);
 
         /**
          * 유저1이 파티3에 신청
          */
         PartyJoinDto partyJoinDto1 = new PartyJoinDto(p3.getPartyId(), PartyJoinStatus.APPLY, "안녕하세요 유저1입니다.");
         PartyDecisionDto partyDecisionDto1 = new PartyDecisionDto(p3.getPartyId(), "유저1", PartyDecision.ACCEPT);
-        partyService.joinParty(partyJoinDto1, u1);
-        partyService.decideUser(partyDecisionDto1, u2);
+        partyService.joinParty(partyJoinDto1, u1.getId());
+        partyService.decideUser(partyDecisionDto1, u2.getId());
 
         /**
          * 유저2이 파티1에 신청
          */
         PartyJoinDto partyJoinDto2 = new PartyJoinDto(p1.getPartyId(), PartyJoinStatus.APPLY, "안녕하세요 유저2입니다.");
         PartyDecisionDto partyDecisionDto2 = new PartyDecisionDto(p1.getPartyId(), "유저2", PartyDecision.ACCEPT);
-        partyService.joinParty(partyJoinDto2, u2);
-        partyService.decideUser(partyDecisionDto2, u1);
+        partyService.joinParty(partyJoinDto2, u2.getId());
+        partyService.decideUser(partyDecisionDto2, u1.getId());
 
         /**
          * 유저3이 파티1, 파티2에 신청
@@ -333,10 +349,10 @@ public class testController {
         PartyJoinDto partyJoinDto3_2 = new PartyJoinDto(p2.getPartyId(), PartyJoinStatus.APPLY, "안녕하세요 유저3입니다.");
         PartyDecisionDto partyDecisionDto3_1 = new PartyDecisionDto(p1.getPartyId(), "유저3", PartyDecision.ACCEPT);
         PartyDecisionDto partyDecisionDto3_2 = new PartyDecisionDto(p2.getPartyId(), "유저3", PartyDecision.ACCEPT);
-        partyService.joinParty(partyJoinDto3_1, u3);
-        partyService.joinParty(partyJoinDto3_2, u3);
-        partyService.decideUser(partyDecisionDto3_1, u1);
-        partyService.decideUser(partyDecisionDto3_2, u1);
+        partyService.joinParty(partyJoinDto3_1, u3.getId());
+        partyService.joinParty(partyJoinDto3_2, u3.getId());
+        partyService.decideUser(partyDecisionDto3_1, u1.getId());
+        partyService.decideUser(partyDecisionDto3_2, u1.getId());
 
         Map<String, Object> ResponseData = new HashMap<>();
         ResponseData.put("유저1의 ID", u1.getId());
